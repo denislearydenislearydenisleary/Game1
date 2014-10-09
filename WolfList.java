702881ea8b7e3f7d18f411d020af6c9200f92696
Andy Sironko <ansironko@vassar.edu>
2014-10-09 14:10:37 -0400

    done with wolf and wolf list i think

--- WolfList.java
+++ WolfList.java
@@ -16,14 +16,39 @@ public class WolfList {
     this.next = next;
   }
   
-  public WolfList addWolf() {
-    int side = (int) (4 * Math.random());
-    return new WolfList(new Wolf.AcWolf(1, 1));
+  public WolfList(WolfList wolflist) {
+    this.wolf = wolflist.wolf;
+    this.next = wolflist.next;
   }
   
-  public WolfList killWolf() {
-    return new WolfList(new Wolf.AcWolf(1, 1));
+  public WolfList addWolf(int cent) {
+    int side = (int) (4 * Math.random());
+    if (side == 0) {
+      return new WolfList(new Wolf.AcWolf((int)
+                                          (cent * Math.random() + (cent / 2)),
+                                          1), this);
+    }
+    else if (side == 1) {
+      return new WolfList(new Wolf.AcWolf((int)
+                                          (cent * Math.random() + (cent / 2)),
+                                          ((2 * cent) - 1)), this);
+    }
+    else if (side == 2) {
+      return new WolfList(new Wolf.AcWolf((int)
+                                          1,
+                                          (int) (cent * Math.random() + (cent / 2))), this);
+    }
+    else {
+      return new WolfList(new Wolf.AcWolf((int)
+                                          ((2 * cent) - 1), (int)
+                                          (cent * Math.random() + (cent / 2))), this);
+    }
+    //return new WolfList(new Wolf.AcWolf(1, 1));
   }
+  
+//  public WolfList killWolf() {
+//    return new WolfList(new Wolf.AcWolf(1, 1));
+//  }
   
   public WolfList getLast() {
     if (this.next == null) {
@@ -46,11 +71,22 @@ public class WolfList {
   }
   
   public WolfList killer(int width, int height) {
-    return new WolfList(new Wolf.AcWolf(1, 1));
+    if (this.wolf.edgeHuh(width, height)) {
+      return new WolfList(this.next).killer(width, height);
+    }
+    else {
+      return new WolfList(this.wolf, this.next.killer(width, height));
+    }
   }
   
   public WolfList mover(int cent) {
-    return new WolfList(new Wolf.AcWolf(1, 1));
+    int speed = (int) (5 * Math.random());
+    if (speed > 2) {
+    return new WolfList (this.wolf.move(cent), this.next.mover(cent));
+    }
+    else {
+      return new WolfList (this.wolf, this.next.mover(cent));
+    }
   }
   
 }
